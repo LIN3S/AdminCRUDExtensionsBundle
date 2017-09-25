59c1401dbb012e8ff6887624ffe98b7b33eaca0e
benatespina <benatespina@gmail.com>
2017-09-25 14:54:28 +0200

    Made compatible with the AdminBundle's v0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ This changelog references the relevant changes done between versions.
 To get the diff for a specific change, go to https://github.com/LIN3S/AdminCRUDExtensionsBundle/commit/XXX where XXX is the change hash 
 To get the diff between two versions, go to https://github.com/LIN3S/AdminCRUDExtensionsBundle/compare/v0.1.0...v0.2.0
 
+* 0.4.0
+    * Made compatible with the AdminBundle's v0.6.
 * 0.3.0
     * Dispatched EntityRemoved event to make more accessible the action type.
 * 0.2.1
--- src/LIN3S/AdminCRUDExtensionsBundle/Action/DeleteActionType.php
+++ src/LIN3S/AdminCRUDExtensionsBundle/Action/DeleteActionType.php
@@ -23,87 +23,40 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
- * Delete action type.
- *
  * @author Beñat Espiña <benatespina@gmail.com>
  */
 final class DeleteActionType implements ActionType
 {
     use EntityId;
 
-    /**
-     * The manager.
-     *
-     * @var ObjectManager
-     */
-    private $manager;
-
-    /**
-     * The session.
-     *
-     * @var Session
-     */
     private $session;
-
-    /**
-     * The Twig instance.
-     *
-     * @var \Twig_Environment
-     */
     private $twig;
-
-    /**
-     * The url generator.
-     *
-     * @var UrlGeneratorInterface
-     */
     private $urlGenerator;
-
-    /**
-     * The event dispatcher.
-     *
-     * @var EventDispatcherInterface
-     */
     private $dispatcher;
 
-    /**
-     * DeleteActionType constructor.
-     *
-     * @param UrlGeneratorInterface    $urlGenerator The url generator
-     * @param ObjectManager            $manager      The manager
-     * @param \Twig_Environment        $twig         The twig instance
-     * @param Session                  $session      The session
-     * @param EventDispatcherInterface $dispatcher   The event dispatcher
-     */
     public function __construct(
         UrlGeneratorInterface $urlGenerator,
-        ObjectManager $manager,
         \Twig_Environment $twig,
         Session $session,
         EventDispatcherInterface $dispatcher
     ) {
         $this->urlGenerator = $urlGenerator;
-        $this->manager = $manager;
         $this->twig = $twig;
         $this->session = $session;
         $this->dispatcher = $dispatcher;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function execute($entity, Entity $config, Request $request, $options = null)
     {
         $id = (string) $this->getEntityId($entity, $config);
-        $repository = $this->manager->getRepository($config->className());
-        $entity = $repository->find($id);
+        $repository = $config->repository();
+        $entity = $repository->find($config, $id);
 
         if (!$entity) {
             throw new NotFoundHttpException();
         }
 
-        $this->manager->remove($entity);
-        $this->manager->flush();
+        $repository->remove($entity);
 
         $this->dispatcher->dispatch(EntityRemoved::name(), new EntityRemoved($id, $config->className()));
 
